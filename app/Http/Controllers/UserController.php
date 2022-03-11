450fec91d446d8b593fac5632d8b271fd15f5b1e
FcoRipJr <fcoripardojr@gmail.com>
2022-03-10 22:35:43 -0400

    test

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,11 @@
+use App\Http\Requests\UserStoreRequest;
+use App\User;
+
+class UsersController extends Controller
+{
+    public function store(UserStoreRequest $request)
+    {
+        return User::query()
+            ->create($request->validated());
+    }
+}
---
+++ app/Http/Requests/UserStoreRequest.php
@@ -0,0 +1,18 @@
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserStoreRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'name'     => 'required|max:100',
+            'email'    => 'required|email',
+            'password' => 'required|confirmed',
+        ];
+    }
+}
