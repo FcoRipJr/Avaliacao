82f7f2a8c3cab9f9ee9bd536d03e8dfb9a96c672
FcoRipJr <fcoripardojr@gmail.com>
2022-03-10 21:38:13 -0400

    criacao crud pessoa

--- app/Http/Controllers/PessoaController.php
+++ app/Http/Controllers/PessoaController.php
@@ -3,8 +3,52 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\PessoaRequest;
+use App\Models\Pessoa;
+use Carbon\Carbon;
 
 class PessoaController extends Controller
 {
-    //
+    public function index(){
+      
+        $pessoas = Pessoa::all();
+        return view('pessoas.index',[
+            'pessoas' => $pessoas
+        ]);
+
+    }
+ 
+    public function create(){
+        return view('pessoas.form');
+    }
+
+    public function store(PessoaRequest $request){
+        $dados = $request->all();
+        Pessoa::create($dados);
+        return redirect()->back()->with('mensagem', 'Registro criado com sucesso!');
+
+    }
+
+    public function edit($id){
+        $pessoa = Pessoa::findorfail($id);
+        return view('pessoa.form')->with('noticia', $pessoa);
+    }
+
+    public function update($id, PessoaRequest $request)
+    {
+        $pessoa = Pessoa::findorfail($id);
+        $dados = $request->all();
+        $pessoa->update($dados);
+        
+        return redirect()->back()->with('mensagem', 'Registro atualizado com sucesso!');
+    }
+
+    public function destroy($id)
+    {
+        $pessoa = Pessoa::findorfail($id);
+        $pessoa->delete();
+
+        return redirect('/pessoas')->with('mensagem', 'Registro excluído com sucesso!');
+    }
+
 }
--- app/Http/Requests/PessoaRequest.php
+++ app/Http/Requests/PessoaRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class PessoaRequest extends FormRequest
 {
@@ -29,6 +30,17 @@ class PessoaRequest extends FormRequest
             'email' => 'required',
             'data_nasc' => 'date_format:d/m/Y',
             'nacionalidade' => 'required'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'nome.required' => 'O campo nome é obrigatório',
+            'cpf.required' => 'O campo cpf é obrigatório',
+            'email.required' => 'O campo email é obrigatório',
+            'data_nasc.required' => 'O campo data da nascimento é obrigatório',
+            'nacionalidade.required' => 'O campo nacionalidade é obrigatório'
         ];
     }
 }
